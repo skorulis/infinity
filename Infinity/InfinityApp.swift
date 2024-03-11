6e17237a7f5f930ffbb7fc813bb2efdfa5f99452
Alex Skorulis <skorulis@gmail.com>
2024-03-11 19:05:48 +1100

    Initial Commit

---
+++ Infinity/InfinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityApp.swift
+//  Infinity
+//
+//  Created by Alexander Skorulis on 11/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct InfinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
